c7ee1191d072dff5328524ebaca8d8c61225c7de
ZukaSutidze <zuka.sutidze@gmail.com>
2023-12-22 21:37:38 +0400

    React 1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='White'>
+        <div className="Blue">
+          <img src="public/qr-code.svg" alt="" />
+        </div>
+        
+        <div className='Texts'>
+          <div className='Toptexts'><p className='Toptext'>Improve your front-end skills by building projects</p></div>
+
+          <div className='Bottomtexts'> <p className='Bottomtext'> Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
